e07b97d021af252b235f623bf0bd5b7229ddfba6
macos <macos@macoss-MacBook-Pro.local>
2022-04-21 21:56:23 +0400

    aloe project

---
+++ js/main.js
@@ -0,0 +1,113 @@
+$('.healthy-life').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    // autoplay:true,
+    // autoplayTimeout:2000,  
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+$('.healthy-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    // autoplay:true,
+    // autoplayTimeout:2000, 
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+$('.bloq-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    // autoplay:true,
+    // autoplayTimeout:2000,  
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+
+$('.brend-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    // autoplay:true,
+    // autoplayTimeout:2000,  
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:4
+        }
+    }
+});
+
+$('.story-carousel').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:true,
+    // autoplay:true,
+    // autoplayTimeout:2000,  
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:9
+        }
+    }
+});
+$('.blog-carousel').owlCarousel({
+    loop:true,
+    margin:16,
+    nav:true,
+    // autoplay:true,
+    // autoplayTimeout:2000,  
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:2 
+        }
+    }
+});
